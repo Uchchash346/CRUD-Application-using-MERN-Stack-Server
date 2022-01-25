fa62e756ef8adade6ec828d4d36af47c030c11e4
Uchchash346 <mru416@gmail.com>
2022-01-26 04:56:13 +0600

    Register part backend completed

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const app = express();
 const mongoose = require('mongoose');
 require('./db/conn')
 const users = require('./models/userSchema');
-
+const cors = require('cors');
+const router = require('./routes/router');
 
 const port = 8003;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(router);
+
 app.listen(port, () => {
     console.log(`server is start port number ${port}`);
 })
+
+//1.26.00
